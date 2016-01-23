32edbbf61708f89514bd9d1fa083b6a205ff14d4
Amaan Cheval <amaan.cheval@gmail.com>
2016-01-23 20:37:32 +0530

    Tada!

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,54 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// Using additional variables in this file because I assume we shouldn't change RAM[0] and RAM[1]
+
+// Set M[R2] = 0
+@R2
+M = 0
+
+// Load the value of R0 into the "step" variable
+@R0
+D = M
+@step
+M = D
+
+// Setup "i" for the loop
+@R1
+D = M
+@i
+M = D
+
+// R2 = 0
+@0
+D = A
+@R2
+M = D
+
+(loop)
+    // Decrement counter
+    @i
+    M = M - 1
+    D = M
+
+    // If counter >= 0, jump to end
+    @end
+    D;JLT
+
+    // Load step into D
+    @step
+    D = M
+
+    // R2 += D
+    @R2
+    M = M + D
+
+    // Jump back to loop
+    @loop
+    0;JMP
+
+
+(end)
+    @end
+    0;JMP
